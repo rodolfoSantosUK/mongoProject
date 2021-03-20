6bdf29a64b8e195c18c71b70a465c3ead16bede6
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-20 10:32:45 -0300

    versao inicial

---
+++ MongoProject/src/main/java/br/com/mongo/MongoProject/MongoProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.mongo.MongoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoProjectApplication.class, args);
+	}
+
+}
---
+++ MongoProject/src/test/java/br/com/mongo/MongoProject/MongoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mongo.MongoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
