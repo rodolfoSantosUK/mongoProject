134b1193359fcadb410d5207b17f1c40d7c2c3e0
rodolfoSantosUK <santoseuro2018@gmail.com>
2021-03-20 15:26:36 -0300

    INSERT

--- MongoProject/src/main/java/br/com/mongo/MongoProject/principal/Principal.java
+++ MongoProject/src/main/java/br/com/mongo/MongoProject/principal/Principal.java
@@ -5,14 +5,24 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
+import java.util.Arrays;
+
 public class Principal {
 
     public static void main(String[] args) {
         MongoClient mongoClient = new MongoClient();
         MongoDatabase mongoDatabase = mongoClient.getDatabase("local");
         MongoCollection<Document> alunos = mongoDatabase.getCollection("alunos");
-        Document aluno =  alunos.find().first();
+        Document aluno = alunos.find().first();
         System.out.println(aluno);
+
+        Document novoAluno =    new Document("nome", "Rodolfo")
+                .append("curso", new Document("nome", "Historia"))
+                .append("notas", Arrays.asList(10, 9, 8))
+                .append("habilidades", Arrays.asList(new Document().append("nome", "Ingles").append("nivel", "Básico"),
+                        new Document().append("nome", "Espanhol").append("nivel", "Básico")));
+        alunos.insertOne(novoAluno);
+
         mongoClient.close();
     }
 
